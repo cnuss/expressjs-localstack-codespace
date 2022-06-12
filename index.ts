c2c4f482c630c940604977a523df54ec0bf4553c
Christian Nuss <cnuss@users.noreply.github.com>
2022-06-12 22:19:21 +0000

    expose port 3000

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import AWS from "aws-sdk";
 
+process.on('SIGINT', function() {
+  console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
+  // some other closing procedures go here
+  process.exit(0);
+});
+
 const BUCKET_NAME = `test-${process.env["CODESPACE_NAME"]}`;
+const KEY_NAME = `test-${process.env["CODESPACE_NAME"]}.txt`
 const TAG_NAME = `X-Last-Updated`;
 
 const s3 = new AWS.S3({
@@ -13,34 +20,33 @@ const s3 = new AWS.S3({
 const app = express();
 
 app.get("/", async (req, res) => {
-  const tagging = await s3.getBucketTagging({ Bucket: BUCKET_NAME }).promise();
-  console.log("!!!! tagging", tagging);
+  const tagging = await s3.getObjectTagging({ Bucket: BUCKET_NAME, Key: KEY_NAME }).promise();
   const tagSet = (tagging.TagSet || []).find((ts) => ts.Key === TAG_NAME);
   if (!tagSet) {
     res.send("Tag is not set\n");
     return;
   }
-  res.send(`Tag was last updated at: ${tagSet.Value}`);
+  res.send(`Tag was last updated at: ${tagSet.Value}\n`);
 });
 
 const updateTag = async () => {
-  console.log("Updating tag on bucket");
   const now = new Date().toISOString();
   const response = await s3
-    .putBucketTagging({
+    .putObjectTagging({
       Bucket: BUCKET_NAME,
+      Key: KEY_NAME,
       Tagging: {
         TagSet: [{ Key: TAG_NAME, Value: now }],
       },
     })
     .promise();
-  console.log("!!! response", response)
   return now;
 };
 
 app.listen(3000, "0.0.0.0", async () => {
   console.log("Server started at 3000");
   await s3.createBucket({ Bucket: BUCKET_NAME }).promise();
+  await s3.putObject({Bucket: BUCKET_NAME, Key: KEY_NAME}).promise();
   setInterval(() => {
     updateTag().then((now) => {
       console.log(`Tag updated to ${now}`);
